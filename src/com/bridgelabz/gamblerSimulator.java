5655f3b77eb7db613e1f73c379604d733a89e6e1
akashbg3010 <akashbpura@gmail.com>
2021-12-16 01:06:49 +0530

    Updated Win or Loose probability of Gambler

--- src/com/bridgelabz/gamblerSimulator.java
+++ src/com/bridgelabz/gamblerSimulator.java
@@ -2,11 +2,17 @@ package com.bridgelabz;
 
 public class gamblerSimulator {
 
-	int totalStake = 100;
-	int betPerGame = 1;
-	
-		public static void main(String[] args) {
-
+	public static void main(String[] args) {
+			int totalStake = 100;
+			int betPerGame = 1;
+			
+			if (Math.random() < 0.5){
+				totalStake++;
+			System.out.println("Gambler Won and his total Stake will be: " + totalStake);
+			}
+			else {
+				totalStake--;
+			System.out.println("Gambler Lost and his total Stake will be: " +totalStake);
+			}
 		}
-
 	}
