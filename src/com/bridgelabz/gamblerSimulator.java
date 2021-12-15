307210a23696b7897341044043c9ffa9635ed88a
akashbg3010 <akashbpura@gmail.com>
2021-12-16 01:24:25 +0530

    Updated Total stake available with gambler after 20 days

--- src/com/bridgelabz/gamblerSimulator.java
+++ src/com/bridgelabz/gamblerSimulator.java
@@ -8,29 +8,34 @@ public class gamblerSimulator {
 	static int betPerGame = 1;
 	static int totalWon = 1;
 	static int totalLoss = 0;
+	static int finalStakeAvailable = 0;
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int cash = totalStake;
-		while (cash > 50 && cash < 150) {
-			int gamePlay = (Math.random() <= 0.5) ? 1 : 2;
-			switch(gamePlay) {
-				case 1:
-					cash = cash + betPerGame;
-					break;
-				case 2:
-					cash = cash - betPerGame;
-					break;
+		for (int i = 1; i <= 20; i++) {
+			int cash = totalStake;
+			while (cash > 50 && cash < 150) {
+				int gamePlay = (Math.random() <= 0.5) ? 1 : 2;
+				switch(gamePlay) {
+					case 1:
+						cash = cash + betPerGame;
+						break;
+					case 2:
+						cash = cash - betPerGame;
+						break;
+				}
+				if (gamePlay == 1) {
+					System.out.println("Gambler Won");
+				}
+				else {
+					System.out.println("Gambler Lost");
+				}
 			}
-			if (gamePlay == 1) {
-				System.out.println("Gambler Won");
+			if (cash == 150 || cash == 50) {
+				System.out.println("Player Quits!");
 			}
-			else {
-				System.out.println("Gambler Lost");
-			}
+				finalStakeAvailable = finalStakeAvailable + cash;
 		}
-		if (cash == 150 || cash == 50) {
-			System.out.println("Player Quits!");
-		}
+		System.out.println("Total Stake remaining with Gambler after 20 days is: "+finalStakeAvailable+" $");
 	}
 }
