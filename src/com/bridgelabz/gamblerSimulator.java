6fcbe6356397e4b44e2c78aa6adc99bb1dcdf7e7
akashbg3010 <akashbpura@gmail.com>
2021-12-16 02:20:24 +0530

    Updated User input for further continuation

--- src/com/bridgelabz/gamblerSimulator.java
+++ src/com/bridgelabz/gamblerSimulator.java
@@ -6,44 +6,59 @@ public class gamblerSimulator {
 
 	static int totalStake = 100;
 	static int betPerGame = 1;
-	static int totalWon = 1;
-	static int totalLoss = 0;
 	static int finalStakeAvailable = 0;
 	static int monthlyDifference = 0;
 
 	public static void main(String[] args) {
+		
+		int totalWon[] = new int[100];
+		int totalLoss[] = new int[100];
+		
 		Scanner s = new Scanner(System.in);
-		for (int i = 1; i <= 20; i++) {
-			int cash = totalStake;
-			while (cash > 50 && cash < 150) {
-				int gamePlay = (Math.random() <= 0.5) ? 1 : 2;
-				switch(gamePlay) {
-					case 1:
-						cash = cash + betPerGame;
-						break;
-					case 2:
-						cash = cash - betPerGame;
-						break;
+		boolean label = true;
+		while (label) {
+			for (int i = 1; i <= 20; i++) {
+				int cash = totalStake;
+				while (cash > 50 && cash < 150) {
+					int gamePlay = (Math.random() <= 0.5) ? 1 : 2;
+					switch(gamePlay) {
+						case 1:
+							cash = cash + betPerGame;
+							break;
+						case 2:
+							cash = cash - betPerGame;
+							break;
+					}
 				}
+				if (cash == 150 || cash == 50) {
+					System.out.println("Player Quits!");
+				}
+				if (cash > 100) {
+					int wonInADay = cash - finalStakeAvailable;
+					System.out.println("Player Won on "+i+" day is: $" + wonInADay);
+				}
+				if (cash < 100) {
+					int lostInADay = cash - finalStakeAvailable;
+					System.out.println("Player Lost on "+i+" day is: $" + lostInADay);
+				}
+				if(cash == 150) {
+			 		totalWon[i] = cash; 
+			 	}
+			 	else if (cash == 50) {
+			 		totalLoss[i] = cash;
+				}
+				finalStakeAvailable = finalStakeAvailable + cash;
 			}
-			if (cash == 150 || cash == 50) {
-				System.out.println("Player Quits!");
-			}
-			if (cash > 100) {
-				int wonInADay = cash - finalStakeAvailable;
-				System.out.println("Player Won on "+i+" day is: $" + wonInADay);
-			}
-			if (cash < 100) {
-				int lostInADay = cash - finalStakeAvailable;
-				System.out.println("Player Lost on "+i+" day is: $" + lostInADay);
-			}
-			 if(cash == 150) {
-				 System.out.println("Day"+i+" is gamblers Luckiest day as he won maximum of "+cash+" $");
+			 System.out.println("Luckiest days for gambler as he WON stake");
+			 for(int i = 1; i <= 20; i++) {
+				 if(totalWon[i] == 150)
+				 System.out.println("In Day"+i+" won "+totalWon[i]+" $");
 			 }
-			 else if (cash == 50) {
-				 System.out.println("Day"+i+" is gamblers Unluckiest day as he lost maximum of "+cash+" RS");
-			 }
-			finalStakeAvailable = finalStakeAvailable + cash;
+			 System.out.println("---------------------------------------------------------------------------");
+			 System.out.println("Unluckiest days for gambler as he LOST maximum stake");
+			 for(int i = 1; i <= 20; i++) {
+				 if(totalLoss[i]== 50)
+				 System.out.println("In Day"+i+" lost "+totalLoss[i]+" $");
 		}
 		System.out.println("Total Stake remaining with Gambler after 20 days is: "+finalStakeAvailable+" $");
 		monthlyDifference = finalStakeAvailable - totalStake * 20;
@@ -54,5 +69,20 @@ public class gamblerSimulator {
 		else {
 			System.out.println("Total Loss of gambler is : "+monthlyDifference+"$");
 		}
+		if(monthlyDifference > 0) {
+			 System.out.println("Do you want to continue playing for next month? If yes, PRESS '1' else, PRESS '2'");
+			 int gamePlay = s.nextInt();
+			 if(gamePlay == 1) {
+				 label = true;
+			 }
+			 else {
+				 label = false;
+				 System.out.println("End of the Game");
+			 }
+		}
+		
+		}
 	}
 }
+		
+	
